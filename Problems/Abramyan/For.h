ff97bc67cc246381950e2b3968fdf7b0430bd915
Ansar <dfqgth400@gmail.com>
2019-05-27 18:30:20 +0600

    Soluton for Problems 10-20 (For)

--- Problems/Abramyan/For.h
+++ Problems/Abramyan/For.h
@@ -70,6 +70,86 @@ void task_9(int a, int b)
         ;
     cout << result;
 }
-
-
+void task_10(int n)
+{
+    float result = 1;
+    for (unsigned i = 2; i <= n; result += 1.0 / i, i++)
+        ;
+    cout << result;
+}
+void task_11(int n)
+{
+    int result = 0;
+    for (unsigned i = 0; i <= n; result += pow(n + i, 2), i++)
+        ;
+    cout << result;
+}
+void task_12(int n)
+{
+    float result = 1;
+    unsigned i1 = 0;
+    for (float i = 1.1; i1 < n; result *= i, i += 0.1, i1++)
+        ;
+    cout << result;
+}
+void task_13(int n)
+{
+    float result = 0;
+    float item = 1.1;
+    int sign = 1;
+    for (unsigned i = 0; i < n; result += item * sign, sign *= -1, i++)
+        ;
+    cout << result;
+}
+void task_14(int n)
+{
+    unsigned result = 0;
+    for (unsigned i = 1; i <= n; result += (2 * i - 1), cout << result << endl, i++)
+        ;
+}
+void task_15(int A, int n)
+{
+    int result = 1;
+    for (unsigned i = 0; i < n; result *= A, i++)
+        ;
+    cout << result;
+}
+void task_16(int A, int n)
+{
+    for (unsigned i = 1; i <= n; cout << pow(A, i) << endl, i++)
+        ;
+}
+void task_17(int A, int n)
+{
+    int result = 0;
+    for (size_t i = 0; i < n; result += pow(A, i), i++)
+        ;
+    cout << result;
+}
+void task_18(int A, int n)
+{
+    int result = 0;
+    int sign = 1;
+    for (size_t i = 0; i < n; result += pow(A, i) * sign, i++, sign *= -1)
+        ;
+    cout << result;
+}
+void task_19(int n)
+{
+    double result = 1;
+    for (unsigned i = 1; i <= n; result *= i, i++)
+        ;
+    cout << result;
+}
+long fact(int n)
+{
+    return n == 1 ? 1 : n * fact(n - 1);
+}
+void task_20(int n)
+{
+    double result = 1;
+    for (unsigned i = 1; i <= n; result += fact(i), i++)
+        ;
+    cout << result;
+}
 } // namespace For
